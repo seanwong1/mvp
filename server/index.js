2da78e7b2aa85a65a6080a3efcf928f2f46b45d9
Sean Wong <sean.uuong@gmail.com>
2023-02-10 12:01:50 -0800

    Add server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const database = require('../database');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
+
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,25 @@ import Staff from './Staff.jsx';
 import hiHat from '../assets/hi_hat.mp3';
 import kick from '../assets/kick.mp3';
 import snare from '../assets/snare.mp3';
+import rest from '../assets/rest.mp3';
 
 class App extends Component {
   state = {
                   //1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4
     kickPattern:   [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
     snarePattern:  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    hiHatPattern:  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+    hiHatPattern:  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
     // kickPattern:  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
     // snarePattern: [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
     // hiHatPattern: [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+    kick: new Audio(kick),
+    snare: new Audio(snare),
+    hiHat: new Audio(hiHat),
+    rest: new Audio(rest),
+    beats: 16,
+    curBeat: 0,
+    tempo: 1,
+    staff: []
   };
 
   // TODO: Slider that sets ms for bpm
@@ -39,9 +48,9 @@ class App extends Component {
     // }
     for (var i = 0; i < this.state.kickPattern.length; i++) {
       if (!this.state.kickPattern[i]) {
-        await this.sleep(150);
+        await this.sleep(100);
       } else {
-        this.playSoundAsync(kick);
+        this.state.kick.play();
       }
     }
 
@@ -50,9 +59,9 @@ class App extends Component {
   async playSnare() {
     for (var i = 0; i < this.state.snarePattern.length; i++) {
       if (!this.state.snarePattern[i]) {
-        await this.sleep(150);
+        await this.sleep(100);
       } else {
-        this.playSoundAsync(snare);
+        this.state.snare.play();
       }
     }
   };
@@ -61,10 +70,10 @@ class App extends Component {
     for (var i = 0; i < this.state.hiHatPattern.length; i++) {
       if (!this.state.hiHatPattern[i]) {
         console.log('**');
-        await this.sleep(150);
+        await this.sleep(100);
       } else {
         console.log('pah');
-        this.playSoundAsync(hiHat);
+        this.state.hiHat.play();
       }
     }
   };
@@ -76,10 +85,99 @@ class App extends Component {
 
   };
 
+    // setTempo() {
+  //   this.state.kick.playbackRate = this.state.tempo;
+  //   this.state.snare.playbackRate = this.state.tempo;
+  //   this.state.hiHat.playbackRate = this.state.tempo;
+  //   this.state.rest.playbackRate = this.state.tempo;
+  // }
+
+  // add = () => {
+  //   this.setTempo();
+  //   for (var i = 0; i < this.state.beats; i++) {
+  //     this.state.staff.push([]);
+  //     if (!this.state.kickPattern[i] && !this.state.snarePattern[i] && !this.state.hiHatPattern[i]) {
+  //       this.state.staff[i].push(this.state.rest);
+  //       this.state.staff[i][0].playbackRate = 2;
+  //     }
+  //     if (this.state.kickPattern[i]) {
+  //       this.state.staff[i].push(this.state.kick);
+  //     }
+  //     if (this.state.snarePattern[i]) {
+  //       this.state.staff[i].push(this.state.snare);
+  //     }
+  //     if (this.state.hiHatPattern[i]) {
+  //       this.state.staff[i].push(this.state.hiHat);
+  //     }
+  //     this.addOnEnd(i);
+  //   }
+  //   console.log(this.state.staff);
+  // }
+
+  // addOnEnd(index) {
+  //   this.state.staff[index][this.state.staff[index].length - 1].addEventListener('ended', this.playNext);
+  // }
+
+  // playNext = () => {
+  //   if (this.state.curBeat < this.state.beats) {
+  //     for (var i = 0; i < this.state.staff[this.state.curBeat].length; i++) {
+  //       this.state.staff[this.state.curBeat][i].play();
+  //     }
+  //     this.state.curBeat++;
+  //   }
+  // }
+
+  // playNext = () => {
+  //   if (this.state.curBeat < this.state.beats) {
+  //     if (!this.state.kickPattern[this.state.curBeat] && !this.state.snarePattern[this.state.curBeat] && !this.state.hiHatPattern[this.state.curBeat]) {
+  //       this.sleep(100);
+  //     }
+  //     if (this.state.kickPattern[this.state.curBeat]) {
+  //       this.state.kick.addEventListener('ended', this.playNext)
+  //       this.state.kick.play();
+  //     }
+  //     if (this.state.snarePattern[this.state.curBeat]) {
+  //       this.state.snare.addEventListener('ended', this.playNext)
+  //       this.state.snare.play();
+  //     }
+  //     if (this.state.hiHatPattern[this.state.curBeat]) {
+  //       this.state.hiHat.addEventListener('ended', this.playNext)
+  //       this.state.hiHat.play();
+  //     }
+  //     this.state.curBeat++;
+  //     console.log(this.state.curBeat);
+  //   }
+  // }
+
+  // playNext = () => {
+  //   if (this.state.curBeat < this.state.beats) {
+  //     if (!this.state.kickPattern[this.state.curBeat] && !this.state.snarePattern[this.state.curBeat] && !this.state.hiHatPattern[this.state.curBeat]) {
+  //       this.sleep(100);
+  //     }
+  //     if (this.state.kickPattern[this.state.curBeat]) {
+  //       this.state.kick.addEventListener('ended', this.playNext)
+  //       this.state.kick.play();
+  //     }
+  //     if (this.state.snarePattern[this.state.curBeat]) {
+  //       this.state.snare.addEventListener('ended', this.playNext)
+  //       this.state.snare.play();
+  //     }
+  //     if (this.state.hiHatPattern[this.state.curBeat]) {
+  //       this.state.hiHat.addEventListener('ended', this.playNext)
+  //       this.state.hiHat.play();
+  //     }
+  //     this.state.curBeat++;
+  //     console.log(this.state.curBeat);
+  //   }
+  // }
+
   render() {
     return (
       <div>
         {/* <Staff /> */}
+        <button onClick={this.add}>
+          Add
+        </button>
         <button onClick={this.playAll}>
           Play
         </button>
